424958f2ae908040bd50b3e4d9ea4ea4642c1bea
tally1728 <tally@zeta.ws>
2021-07-22 22:25:48 +0400

    2. Programming a Guessing Game (rev.2): Generating a Secret Number modified: /ch02-guessing-game-tutorial/Cargo.toml modified: /ch02-guessing-game-tutorial/src/main.rs

--- ch02-guessing-game-tutorial/src/main.rs
+++ ch02-guessing-game-tutorial/src/main.rs
@@ -1,3 +1,5 @@
+// randクレート（乱数生成器）を導入
+use rand::Rng;
 // 標準ライブラリの入出力ライブラリを導入
 use std::io;
 
@@ -5,6 +7,20 @@ use std::io;
 fn main() {
     // 文字列 "Guess the number!" を表示するマクロ
     println!("Guess the number!");
+
+    // 1. 不変変数 secret_number を定義
+    // 2. rand::thread_rng() 関数を呼び出して、
+    // ThreadRng オブジェクト（スレッドに固有な擬似乱数生成器）を生成
+    // Function rand::thread_rng() -> ThreadRng
+    // 3. Trait rand::Rng で定義された ThreadRng オブジェクトの
+    // gen_range メソッドを呼び出して、1〜100の擬似乱数を生成
+    // fn gen_range<T: PartialOrd + SampleUniform>(&mut self, low: T, high: T) -> T
+    // 4. 生成された値を secret_number へ代入
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    // 文字列 "The secret number is: " と secret_number の値を表示するマクロ
+    // {} がプレースホルダーとなっている。
+    println!("The secret number is: {}", secret_number);
 
     // 文字列 "Please input your guess." を表示するマクロ
     println!("Please input your guess.");
