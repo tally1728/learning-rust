f85cc964c0c48b0c5f971f50d6fa3022008097fe
tally1728 <tally@zeta.ws>
2021-07-23 17:16:44 +0400

    Ch.2. Programming a Guessing Game (rev.4.1)
     Allowing Multiple Guesses with Looping
     - modified: /ch02-guessing-game-tutorial/src/main.rs
       - add loop keywork

--- ch02-guessing-game-tutorial/src/main.rs
+++ ch02-guessing-game-tutorial/src/main.rs
@@ -24,73 +24,76 @@ fn main() {
     // {} がプレースホルダーとなっている。
     println!("The secret number is: {}", secret_number);
 
-    // 文字列 "Please input your guess." を表示するマクロ
-    println!("Please input your guess.");
+    // 無限ループ開始
+    loop {
+        // 文字列 "Please input your guess." を表示するマクロ
+        println!("Please input your guess.");
 
-    // 1. 可変変数 guess を定義
-    // 2. String 型の関連関数（スタティックメソッド）new を呼び出して、
-    // 空の String オブジェクト（空の文字列）を生成
-    // pub const fn new() -> String
-    // 3. その String オブジェクトを guess へ代入
-    let mut guess = String::new();
+        // 1. 可変変数 guess を定義
+        // 2. String 型の関連関数（スタティックメソッド）new を呼び出して、
+        // 空の String オブジェクト（空の文字列）を生成
+        // pub const fn new() -> String
+        // 3. その String オブジェクトを guess へ代入
+        let mut guess = String::new();
 
-    // IOモジュール（入出力ライブラリ）の stdin 関数を呼び出して、
-    // 標準入力へのハンドルを表す Stdin オブジェクトを生成
-    // pub fn stdin() -> Stdin
-    io::stdin()
-        // Stdin オブジェクトの read_line メソッドを呼び出して、
-        // ユーザからの入力を受け付ける。
-        // 引数として変数 guess への可変な参照を渡すことで、
-        // ユーザ入力が変数 guess に追記される。
-        // 処理の成功または失敗を表す Result オブジェクトを返す。
-        // pub fn read_line(&self, buf: &mut String) -> Result<usize>
-        .read_line(&mut guess)
-        // Result オブジェクトの expect メソッドを呼び出す。
-        // もし read_line の処理が失敗した場合、
-        // 文字列 "Failed to read line" とエラー内容を含む
+        // IOモジュール（入出力ライブラリ）の stdin 関数を呼び出して、
+        // 標準入力へのハンドルを表す Stdin オブジェクトを生成
+        // pub fn stdin() -> Stdin
+        io::stdin()
+            // Stdin オブジェクトの read_line メソッドを呼び出して、
+            // ユーザからの入力を受け付ける。
+            // 引数として変数 guess への可変な参照を渡すことで、
+            // ユーザ入力が変数 guess に追記される。
+            // 処理の成功または失敗を表す Result オブジェクトを返す。
+            // pub fn read_line(&self, buf: &mut String) -> Result<usize>
+            .read_line(&mut guess)
+            // Result オブジェクトの expect メソッドを呼び出す。
+            // もし read_line の処理が失敗した場合、
+            // 文字列 "Failed to read line" とエラー内容を含む
+            // パニックメッセージを表示して、プログラムを終了させる。
+            // もし read_line の処理が成功した場合、
+            // Ok 列挙子が保持する値、つまりユーザが入力したデータのバイト数を返す。
+            // pub fn expect(self, msg: &str) -> T
+            .expect("Failed to read line");
+
+        // 1. u32型の不変変数 guess を（再）定義 → シャドーイング
+        // 2. 元の guess （文字列）の trim メソッドを呼び出して、
+        // 文字列から冒頭と末尾の空白を除去する。
+        // pub fn trim(&self) -> &str
+        // 3. parse メソッドを呼び出して、文字列を解析して別の型に変換する。
+        // 先に u32 型がしていされているので、u32 型に変換しようと試みる。
+        // 変換結果は Result オブジェクトとして返される。
+        // pub fn parse<F>(&self) -> Result<F, <F as FromStr>::Err>
+        // 4. Result オブジェクトの expect メソッドを呼び出す。
+        // もし型変換処理が失敗した場合、
+        // 文字列 "Please type a number!" とエラー内容を含む
         // パニックメッセージを表示して、プログラムを終了させる。
-        // もし read_line の処理が成功した場合、
-        // Ok 列挙子が保持する値、つまりユーザが入力したデータのバイト数を返す。
+        // もし型変換処理が成功した場合、
+        // Ok 列挙子が保持する値、つまり u32 型に変換された値を返す。
         // pub fn expect(self, msg: &str) -> T
-        .expect("Failed to read line");
+        // 5. u32 型に変換された値が guess へ代入される。
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    // 1. u32型の不変変数 guess を（再）定義 → シャドーイング
-    // 2. 元の guess （文字列）の trim メソッドを呼び出して、
-    // 文字列から冒頭と末尾の空白を除去する。
-    // pub fn trim(&self) -> &str
-    // 3. parse メソッドを呼び出して、文字列を解析して別の型に変換する。
-    // 先に u32 型がしていされているので、u32 型に変換しようと試みる。
-    // 変換結果は Result オブジェクトとして返される。
-    // pub fn parse<F>(&self) -> Result<F, <F as FromStr>::Err>
-    // 4. Result オブジェクトの expect メソッドを呼び出す。
-    // もし型変換処理が失敗した場合、
-    // 文字列 "Please type a number!" とエラー内容を含む
-    // パニックメッセージを表示して、プログラムを終了させる。
-    // もし型変換処理が成功した場合、
-    // Ok 列挙子が保持する値、つまり u32 型に変換された値を返す。
-    // pub fn expect(self, msg: &str) -> T
-    // 5. u32 型に変換された値が guess へ代入される。
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        // 文字列 "You guessed: " と guess の値を表示するマクロ
+        // {} がプレースホルダーとなっている。
+        println!("You guessed: {}", guess);
 
-    // 文字列 "You guessed: " と guess の値を表示するマクロ
-    // {} がプレースホルダーとなっている。
-    println!("You guessed: {}", guess);
-
-    // guess の cmp メソッドを呼び出して、secret_number との大小関係を比較する。
-    // 比較結果は Ordering オブジェクト（列挙型）として出力される。
-    // 例えば guess が secret_number より大きい場合は、
-    // 比較結果として値 Ordering::Greater を返す。
-    // pub fn cmp(&self, other: &u32) -> Ordering
-    // match 式で比較結果に該当する処理を実行する。
-    match guess.cmp(&secret_number) {
-        // 比較結果が Ordering::Less の場合、
-        // つまり guess が secret_number より小さい場合、文字列 "Too small!" を出力
-        Ordering::Less => println!("Too small!"),
-        // 比較結果が Ordering::Greater の場合、
-        // つまり guess が secret_number より大きい場合、文字列 "Too big!" を出力
-        Ordering::Greater => println!("Too big!"),
-        // 比較結果が Ordering::Equal の場合、
-        // つまり guess と secret_number が等しい場合、文字列 "You win!" を出力
-        Ordering::Equal => println!("You win!"),
+        // guess の cmp メソッドを呼び出して、secret_number との大小関係を比較する。
+        // 比較結果は Ordering オブジェクト（列挙型）として出力される。
+        // 例えば guess が secret_number より大きい場合は、
+        // 比較結果として値 Ordering::Greater を返す。
+        // pub fn cmp(&self, other: &u32) -> Ordering
+        // match 式で比較結果に該当する処理を実行する。
+        match guess.cmp(&secret_number) {
+            // 比較結果が Ordering::Less の場合、
+            // つまり guess が secret_number より小さい場合、文字列 "Too small!" を出力
+            Ordering::Less => println!("Too small!"),
+            // 比較結果が Ordering::Greater の場合、
+            // つまり guess が secret_number より大きい場合、文字列 "Too big!" を出力
+            Ordering::Greater => println!("Too big!"),
+            // 比較結果が Ordering::Equal の場合、
+            // つまり guess と secret_number が等しい場合、文字列 "You win!" を出力
+            Ordering::Equal => println!("You win!"),
+        }
     }
 }
