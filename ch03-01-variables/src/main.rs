751f219732198530cbf714c03dbff15476b555e8
tally1728 <tally@zeta.ws>
2021-08-05 13:33:42 +0400

    Ch.3.1 Variables and Mutability - Example-4
    add another example of shadowing of variables

--- ch03-01-variables/src/main.rs
+++ ch03-01-variables/src/main.rs
@@ -5,4 +5,13 @@ fn main() {
     println!("The value of x is: {}", x);
     let x = x * 2;
     println!("The value of x is: {}", x);
+
+    // OK
+    let spaces = "   ";
+    let spaces = spaces.len();
+    println!("length of spaces is: {}", spaces);
+
+    // NG
+    // let mut spaces = "   ";
+    // spaces = spaces.len(); // error[E0308]: mismatched types
 }
