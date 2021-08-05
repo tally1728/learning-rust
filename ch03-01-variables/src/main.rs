ca01f801c3bcf7d675084add9791dbcb54f2513d
tally1728 <tally@zeta.ws>
2021-08-05 12:59:34 +0400

    Ch.3.1 Variables and Mutability - Example-3
    try shadowing of variables

--- ch03-01-variables/src/main.rs
+++ ch03-01-variables/src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
-    let mut x = 5;
+    let x = 5;
     println!("The value of x is: {}", x);
-    x = 6;
+    let x = x + 1;
+    println!("The value of x is: {}", x);
+    let x = x * 2;
     println!("The value of x is: {}", x);
 }
