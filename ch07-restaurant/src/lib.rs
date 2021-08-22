481094d931c2f118bbe88af9c137ceb68f31df34
tally1728 <tally@zeta.ws>
2021-08-22 20:15:15 +0400

    Ch.07.3 Paths - Listing 7-9 pub struct

--- ch07-restaurant/src/lib.rs
+++ ch07-restaurant/src/lib.rs
@@ -1,4 +1,4 @@
-// Listing 7-7: Absolute path & Relative path
+// Ch.07.3 Paths - Listing 7-7: Absolute path & Relative path
 mod listing_7_7 {
     mod front_of_house {
 
@@ -28,7 +28,7 @@ mod listing_7_7 {
     }
 }
 
-// Listing 7-8: Relative path w/ super
+// Ch.07.3 Paths - Listing 7-8: Relative path w/ super
 mod listing_7_8 {
     fn serve_order() {}
 
@@ -38,5 +38,37 @@ mod listing_7_8 {
             super::serve_order();
         }
         fn cook_order() {}
+    }
+}
+
+// Ch.07.3 Paths - Listing 7-9: pub struct
+mod listing_7_9 {
+    mod back_of_house {
+        pub struct Breakfast {
+            pub toast: String,
+            seasonal_fruit: String,
+        }
+
+        impl Breakfast {
+            pub fn summer(toast: &str) -> Breakfast {
+                Breakfast {
+                    toast: String::from(toast),
+                    seasonal_fruit: String::from("peaches"),
+                }
+            }
+        }
+    }
+
+    pub fn eat_at_restaurant() {
+        // Order a breakfast in the summer with Rye toast
+        let mut meal = back_of_house::Breakfast::summer("Rye");
+
+        // Change our mind about what bread we'd like
+        meal.toast = String::from("Wheat");
+
+        println!("I'd like {} toast please", meal.toast);
+
+        // error[E0616]: field `seasonal_fruit` of struct `Breakfast` is private
+        //meal.seasonal_fruit = String::from("blueberries");
     }
 }
