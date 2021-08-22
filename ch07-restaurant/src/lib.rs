591c8d0eb42244d81ec7f6dbaed6a0f99514664a
tally1728 <tally@zeta.ws>
2021-08-22 19:56:16 +0400

    Ch.07.3 Paths - Listing 7-8

--- ch07-restaurant/src/lib.rs
+++ ch07-restaurant/src/lib.rs
@@ -1,24 +1,42 @@
-mod front_of_house {
+// Listing 7-7: Absolute path & Relative path
+mod listing_7_7 {
+    mod front_of_house {
 
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
+        pub mod hosting {
+            pub fn add_to_waitlist() {}
 
-        fn seat_at_table() {}
+            fn seat_at_table() {}
+        }
+
+        mod serving {
+            fn cook_order() {}
+
+            fn take_order() {}
+
+            fn serve_order() {}
+
+            fn take_payment() {}
+        }
     }
 
-    mod serving {
-        fn take_order() {}
+    pub fn eat_at_restaurant() {
+        // Absolute path
+        crate::listing_7_7::front_of_house::hosting::add_to_waitlist();
 
-        fn serve_order() {}
-
-        fn take_payment() {}
+        // Relative path
+        front_of_house::hosting::add_to_waitlist();
     }
 }
 
-pub fn eat_at_restaurant() {
-    // Absolute path
-    crate::front_of_house::hosting::add_to_waitlist();
+// Listing 7-8: Relative path w/ super
+mod listing_7_8 {
+    fn serve_order() {}
 
-    // Relative Path
-    front_of_house::hosting::add_to_waitlist();
+    mod back_of_house {
+        fn fix_incorrect_order() {
+            cook_order();
+            super::serve_order();
+        }
+        fn cook_order() {}
+    }
 }
