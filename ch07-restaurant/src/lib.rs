c734223c9d9f444190a2cb9f52ac6176e08c6c78
tally1728 <tally@zeta.ws>
2021-08-22 19:16:45 +0400

    Ch.07.3 Paths - Listing 7-7

--- ch07-restaurant/src/lib.rs
+++ ch07-restaurant/src/lib.rs
@@ -1,6 +1,7 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
 
         fn seat_at_table() {}
     }
@@ -12,4 +13,12 @@ mod front_of_house {
 
         fn take_payment() {}
     }
+}
+
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative Path
+    front_of_house::hosting::add_to_waitlist();
 }
