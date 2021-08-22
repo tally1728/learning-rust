5a0e4b1ffbe2201c433364d2b3d3994f2f2c3d7e
tally1728 <tally@zeta.ws>
2021-08-22 20:19:25 +0400

    Ch.07.3 Paths - Listing 7-10 pub enum

--- ch07-restaurant/src/lib.rs
+++ ch07-restaurant/src/lib.rs
@@ -72,3 +72,18 @@ mod listing_7_9 {
         //meal.seasonal_fruit = String::from("blueberries");
     }
 }
+
+// Ch.07.3 Paths - Listing 7-10: pub enum
+mod listing_7_10 {
+    mod back_of_house {
+        pub enum Appetizer {
+            Soup,
+            Salad,
+        }
+    }
+
+    pub fn eat_at_restaurant() {
+        let order1 = back_of_house::Appetizer::Soup;
+        let order2 = back_of_house::Appetizer::Salad;
+    }
+}
