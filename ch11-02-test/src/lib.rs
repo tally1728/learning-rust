b72b0ea408dc7f04eac34c69366eb0792239f6de
tally1728 <tally@zeta.ws>
2021-09-01 18:32:42 +0400

    Ch.11.02 Controlling How Tests Are Run

---
+++ ch11-02-test/src/lib.rs
@@ -0,0 +1,55 @@
+fn prints_and_returns_10(a: i32) -> i32 {
+    println!("I got the value {}", a);
+    10
+}
+
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+#[cfg(test)]
+mod tests {
+    mod tests_1 {
+        use crate::prints_and_returns_10;
+
+        #[test]
+        fn this_test_will_pass() {
+            let value = prints_and_returns_10(4);
+            assert_eq!(10, value);
+        }
+
+        #[test]
+        fn this_test_will_fail() {
+            let value = prints_and_returns_10(8);
+            assert_eq!(5, value);
+        }
+    }
+
+    mod tests_2 {
+        use crate::add_two;
+
+        #[test]
+        fn add_two_and_two() {
+            assert_eq!(4, add_two(2));
+        }
+
+        #[test]
+        fn add_three_and_two() {
+            assert_eq!(5, add_two(3));
+        }
+
+        #[test]
+        fn one_hundred() {
+            assert_eq!(102, add_two(100));
+        }
+    }
+
+    mod tests_3 {
+        #[test]
+        #[ignore]
+        fn expensive_test() {
+            // 実行に1時間かかるコード
+            // code that takes an hour to run
+        }
+    }
+}
