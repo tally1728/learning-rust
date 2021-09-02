b817077a39de05db5f10908d981ab1609d63e986
tally1728 <tally@zeta.ws>
2021-09-02 23:34:16 +0400

    Ch.12.04 the Library’s Functionality with TDD

--- ch12-minigrep/src/lib.rs
+++ ch12-minigrep/src/lib.rs
@@ -2,6 +2,7 @@ use std::fs::File;
 // to get read_to_string() method enabled on File Struct
 use std::io::Read;
 
+#[derive(PartialEq, Debug)]
 pub struct Config {
     pub query: String,
     pub filename: String,
@@ -33,7 +34,99 @@ pub fn run(config: Config) -> std::io::Result<()> {
     // fn read_to_string(&mut self, buf: &mut String) -> Result<usize>
     f.read_to_string(&mut contents)?;
 
-    println!("With text:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
 
     Ok(())
+}
+
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+
+    result
+}
+
+////////////////////////////////////////////////////////////
+// Tests
+//
+#[cfg(test)]
+mod test {
+    use super::run;
+    use super::search;
+    use super::Config;
+
+    #[test]
+    fn config_new_with_1arg() {
+        let query = "you";
+        let args = vec![String::from("cmd"), String::from(query)];
+
+        assert_eq!(Config::new(&args), Err("not enough arguments"));
+    }
+
+    #[test]
+    fn config_new_with_2args() {
+        let query = "you";
+        let filename = "poem.txt";
+        let args = vec![
+            String::from("cmd"),
+            String::from(query),
+            String::from(filename),
+        ];
+
+        assert_eq!(
+            Config::new(&args),
+            Ok(Config {
+                query: String::from(query),
+                filename: String::from(filename)
+            })
+        );
+    }
+
+    #[test]
+    fn run_with_an_existing_file() -> std::io::Result<()> {
+        let query = "you";
+        let filename = "poem.txt";
+        let config = Config {
+            query: String::from(query),
+            filename: String::from(filename),
+        };
+
+        run(config)
+        //assert_eq!(run(config), Ok(()));
+    }
+
+    #[test]
+    fn run_with_no_existing_file() {
+        let query = "you";
+        let filename = "hoge.txt";
+        let config = Config {
+            query: String::from(query),
+            filename: String::from(filename),
+        };
+
+        match run(config) {
+            Ok(_) => panic!("run() returns Ok!"),
+            Err(e) => {
+                assert_eq!(e.kind(), std::io::ErrorKind::NotFound);
+            }
+        }
+    }
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
--- ch12-minigrep/src/main.rs
+++ ch12-minigrep/src/main.rs
@@ -16,8 +16,8 @@ fn main() {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
+    //println!("Searching for {}", config.query);
+    //println!("In file {}", config.filename);
 
     if let Err(e) = run(config) {
         println!("Application error: {}", e);
